811955f4f0ec40464a2d9193c3802e58a2d34742
elang.nugraha <>
2024-03-19 09:20:38 +0700

    first commit

---
+++ src/main/java/main/Perpustakaan_app_staging.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author elang.nugraha
+ */
+public class Perpustakaan_app_staging {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
